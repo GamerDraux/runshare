0a7788bbd4c3935cfad115109960a54f76c59660
Andy Koch <akandrewkoch@gmail.com>
2020-07-10 18:01:20 -0500

    setup base landing page, body CSS and home link with logo

--- src/main/java/com/akproject/runshare/controllers/HomeController.java
+++ src/main/java/com/akproject/runshare/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.akproject.runshare.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,6 +9,7 @@ public class HomeController {
 
     @RequestMapping("")
     private String displayIndexPage(Model model){
+        model.addAttribute("title", "Home");
         return "index";
     }
 }
