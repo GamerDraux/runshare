3bd8660f88999deb9f1cb3b7cac8ecd2ef14d57b
Andy Koch <akandrewkoch@gmail.com>
2020-08-12 18:04:09 -0500

    -fixed navButton to cycle open/closed instead of just open

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,12 +1,20 @@
+let navClosed = true;
+
 function openNav(){
-    document.getElementById("sideNav").style.width =
+    if (navClosed===true)
+    {document.getElementById("sideNav").style.width =
     "250px";
     document.getElementById("main").style
     .marginLeft="250px";
+    navClosed = false
+    } else{
+        closeNav();
+    }
 }
 
 function closeNav() {
     document.getElementById("sideNav").style.width = "0";
     document.getElementById("main").style
     .marginLeft="0";
+    navClosed= true;
 }
